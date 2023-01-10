befc7567b0edf663379ffffd10a271206bbd6ae6
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2023-01-10 22:03:47 +0100

    Refactor OCaml code

--- src/parse_goal.ml
+++ src/parse_goal.ml
@@ -12,6 +12,44 @@ type rule =
   { direction : direction;
     theorem : string; }
 
+type proof_seq = { seq : rule list; } [@@boxed]
+
+let term_to_str env trm sigma =
+  let constr = EConstr.to_constr sigma trm in
+  let pp_goal = Printer.pr_constr_env env sigma constr in
+  Pp.string_of_ppcmds pp_goal
+
+(* Parse goal represented as a Ecostr to
+   an s expression. Assumes that the term 
+   contains only Applications and Variables *)
+let rec goal_to_s env trm sigma = 
+  match EConstr.kind sigma trm with
+  | App (f, args) -> 
+    let f_name = term_to_str env f sigma in
+    (* The first element of the list is a
+    type parameter and for now I just drop it *)
+    let args = Array.sub args 1 (Array.length args - 1) in
+    let args_repr = 
+      Array.to_list args
+      |> List.map (fun arg -> goal_to_s env arg sigma)
+    in
+    Application (f_name, args_repr)
+  | Var n -> Symbol (Names.Id.to_string n)
+  | _ -> raise (Goal_parse_exp "Invalid goal term.") 
+
+(* Split the statement into a pair of lhs and rhs,
+   throw Goal_parse_exp if the amount of 
+   arguments is not equal to 2 *)
+let split_goal expr =
+  match expr with
+  | Application (f, args) -> 
+    if List.length args != 2 then
+      raise (Goal_parse_exp "Invalid goal term.")
+    else
+      (List.nth args 0, List.nth args 1)
+  | _ -> raise (Goal_parse_exp "Invalid goal term.")
+
+(* Utility functions *)
 let rule_to_string rule = 
   let dir = 
     match rule.direction with
@@ -19,13 +57,6 @@ let rule_to_string rule =
     | Backward -> "backward"
   in
   "(" ^ dir ^ " " ^ rule.theorem ^ ")"
-
-type proof_seq = { seq : rule list; } [@@boxed]
-
-let term_to_str env trm sigma =
-  let constr = EConstr.to_constr sigma trm in
-  let pp_goal = Printer.pr_constr_env env sigma constr in
-  Pp.string_of_ppcmds pp_goal
 
 let rec s_expr_to_string expr = 
   match expr with
@@ -36,27 +67,3 @@ let rec s_expr_to_string expr =
       |> String.concat " "
     in
     "(" ^ f ^ " " ^ args_repr ^ ")"
-
-let rec goal_to_s env trm sigma = 
-  match EConstr.kind sigma trm with
-  | App (f, args) -> 
-    let f_name = term_to_str env f sigma in
-    (* Apparently, the first argument of Application is (relation A), 
-       we will just drop it for now. *)
-    let args = Array.sub args 1 (Array.length args - 1) in
-    let args_repr = 
-      Array.to_list args
-      |> List.map (fun arg -> goal_to_s env arg sigma)
-    in
-    Application (f_name, args_repr)
-  | Var n -> Symbol (Names.Id.to_string n)
-  | _ -> raise (Goal_parse_exp "Invalid goal term.") 
-
-let split_goal expr =
-  match expr with
-  | Application (f, args) -> 
-    if List.length args != 2 then
-      raise (Goal_parse_exp "Invalid goal term.")
-    else
-      (List.nth args 0, List.nth args 1)
-  | _ -> raise (Goal_parse_exp "Invalid goal term.")
--- src/parse_goal.mli
+++ src/parse_goal.mli
@@ -15,16 +15,14 @@ type rule =
   { direction : direction;
     theorem : string; }
 
-val rule_to_string : 
-  rule -> 
-  string
-
 type proof_seq = { seq : rule list; } [@@boxed]
 
-val s_expr_to_string : 
-  goal_s_expr -> 
+val term_to_str :
+  env ->
+  EConstr.t ->
+  evar_map ->
   string
-  
+
 val goal_to_s : 
   env -> 
   EConstr.t -> 
@@ -34,3 +32,11 @@ val goal_to_s :
 val split_goal : 
   goal_s_expr -> 
   goal_s_expr * goal_s_expr
+
+val rule_to_string : 
+  rule -> 
+  string
+
+val s_expr_to_string : 
+  goal_s_expr -> 
+  string
